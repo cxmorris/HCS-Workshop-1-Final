98abe4692fdc935cd2e260febc8b2235e2f7a1ac
cxmorris <66590011+cxmorris@users.noreply.github.com>
2020-10-04 14:44:12 -0400

    Updates

---
+++ precache-manifest.53adff1645f0468b0bd1b1a24421bc43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffe70d38942a62d10b9197cd5bc9825",
+    "url": "/HCS-Workshop-1-Final/index.html"
+  },
+  {
+    "revision": "ef1a42224928b8c68a9b",
+    "url": "/HCS-Workshop-1-Final/static/css/main.0c465f27.chunk.css"
+  },
+  {
+    "revision": "0ca4c79425a0f63d59a0",
+    "url": "/HCS-Workshop-1-Final/static/js/2.487dff4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HCS-Workshop-1-Final/static/js/2.487dff4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef1a42224928b8c68a9b",
+    "url": "/HCS-Workshop-1-Final/static/js/main.9ce008fb.chunk.js"
+  },
+  {
+    "revision": "59ebe2346a4f21a752d7",
+    "url": "/HCS-Workshop-1-Final/static/js/runtime-main.dedb9f8e.js"
+  },
+  {
+    "revision": "ebcf0abc9534a9612ed1f90afc4b89b6",
+    "url": "/HCS-Workshop-1-Final/static/media/bighappy.ebcf0abc.jpeg"
+  }
+]);
